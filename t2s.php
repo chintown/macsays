749318a524c842b0f80620e4fa1a68259ee2d07f
chintown <chintown@gmail.com>
2011-12-17 20:39:59 +0800

    say command requires utf8 content

--- t2s.php
+++ t2s.php
@@ -1,4 +1,7 @@
 <?php
+	function to_utf8($str){
+		return mb_convert_encoding($str,"UTF-8", "BIG5");
+	}
 	$content = "";
 	if (isset($_GET['s'])) {
 		$content = $_GET['s'];
@@ -12,10 +15,12 @@
 	}
 	$fn = '/tmp/macsays.bak';
 	$fp = fopen($fn, 'w');
+	$content = to_utf8($content);
 	fwrite($fp, $content);
 	fclose($fp);
 
 	$cmd = "say -f $fn";
+	echo "<!-- $cmd -->";
 	shell_exec($cmd);
 ?>
 <script>window.close()</script>
