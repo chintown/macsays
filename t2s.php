748238621c685b5eebc9d542b44b5ce9cd820a5d
chintown <chintown@gmail.com>
2011-12-17 18:01:11 +0800

    prevent command injection

--- t2s.php
+++ t2s.php
@@ -1,15 +1,21 @@
 <?php
-	#$content = 'hello "Mike Chen"';
-	if (isset($_GET['s']) && '' != $_GET['s']) {
+	$content = "";
+	if (isset($_GET['s'])) {
 		$content = $_GET['s'];
-	} else if (isset($_POST['s']) && '' != $_POST['s']) {
+	} else if (isset($_POST['s'])) {
 		$content = $_POST['s'];
-	} else {
+	} 
+
+	if ($content == "") {
 		echo 'input $s';
 		exit(1);
 	}
-	$content = str_replace('"', '\"', $content);
-	$cmd = 'say "'.$content.'"';
+	$fn = '/tmp/macsays.bak';
+	$fp = fopen($fn, 'w');
+	fwrite($fp, $content);
+	fclose($fp);
+
+	$cmd = "say -f $fn";
 	shell_exec($cmd);
-//<script>window.close()</script>
 ?>
+<script>window.close()</script>
